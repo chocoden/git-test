5db3dc91c86c9aa7bb8e3570cf440c89ddd24087
chocoden <kazuma.sf.soccer@gmail.com>
2024-06-10 22:12:44 +0900

    カリキュラム9-2

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,6 +10,7 @@
         <h1 class='title'>
             {{ $post->title }}
         </h1>
+        <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
         <div class='content'>
             <div class='content_post'>
                     <h3>本文</h3>
@@ -23,5 +24,6 @@
         <div class='footer'>
             <a href="/">戻る</a>
         </div>
+        
     </body>
 </html>
