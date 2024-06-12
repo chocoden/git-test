b394949f3cdb66a8440a6b33822226d469535575
chocoden <kazuma.sf.soccer@gmail.com>
2024-06-12 15:38:21 +0900

    カリキュラム9-3

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,24 +6,30 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
-    <body>
-        <h1 class='title'>
-            {{ $post->title }}
-        </h1>
-        <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
-        <div class='content'>
-            <div class='content_post'>
-                    <h3>本文</h3>
-                    <p class='body'>{{ $post->body }}</p>
+    <x-app-layout>
+        <x-slot name="header">
+           
+        </x-slot>
+            <body>
+            <h1 class='title'>
+                {{ $post->title }}
+            </h1>
+            <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
+            <div class='content'>
+                <div class='content_post'>
+                        <h3>本文</h3>
+                        <p class='body'>{{ $post->body }}</p>
+                </div>
+                
+            </div>
+            <div class='edit'>
+                <a href="/posts/{{ $post->id }}/edit">edit</a>
+            </div>
+            <div class='footer'>
+                <a href="/">戻る</a>
             </div>
             
-        </div>
-        <div class='edit'>
-            <a href="/posts/{{ $post->id }}/edit">edit</a>
-        </div>
-        <div class='footer'>
-            <a href="/">戻る</a>
-        </div>
-        
-    </body>
+        </body>
+    </x-app-layout>
+    
 </html>
